0474dc52efce5a5de6e4cf54eb4bbe1664e272c8
sheptianbagja <sheptianbagjautama@gmail.com>
2020-07-02 22:28:06 +0700

    Routing
    Belajar Routing

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +6,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  // Agar dapat digunakan di parent component, custom event yang 
-  // akan digunakan oleh parent component
- @Output() featureSelected = new EventEmitter<string>();
-
- onSelect(feature:string){
-  //  Mempublish argument feature ke luar component
-   this.featureSelected.emit(feature);
- }
 
 }
