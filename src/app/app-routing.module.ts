49e307203e5c5406fe4d7faa8b3a899195c0ec64
sheptianbagja <sheptianbagjautama@gmail.com>
2020-07-13 00:32:11 +0700

    Modules & Optimizing Lady Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/auth.guard';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [RecipesResolverService],
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [RecipesResolverService],
-      },
-    ],
-  },
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'auth', component: AuthComponent },
+
+  // Lazy Loading
+  // Jika tidak ingin menggunakan lazy loading komen kode di bawah ini
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(r => r.RecipesModule )},
+  { path: 'shopping-list', loadChildren:() => import('./shopping-list/shopping-list.module').then(s => s.ShoppingListModule )},
+  { path: 'auth', loadChildren:() => import('./auth/auth.module').then(a => a.AuthModule )},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes,{ preloadingStrategy:PreloadAllModules}
+  )],
+
+  // Tanpa menggunakan lazy loading
+  // imports: [RouterModule.forRoot(appRoutes)],
   // Agar dapat digunakan di file lain, lebih tepatnya di App Module
   exports: [RouterModule],
 })
